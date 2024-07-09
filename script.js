bf6a60d170ffefaa65dcde52b70c582be99882be
Erroneous User <adarshkumar.19200019@gmail.com>
2024-07-10 02:18:29 +0530

    Improved code

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const buttons = Array.from(document.getElementsByClassName("a"));
 const screen = document.getElementById("screen");
 let value = 1;
 let result=1;
+let fact = 1;
+let nums= 1;
 
 function input() {
   buttons.forEach((button, index) => {
@@ -32,69 +34,84 @@ function input() {
   });
 }
 input();
-
 function calculation(val) {
-  if (val.includes("sin(")) {
-    val = val.replace("sin(", "Math.sin(");
-  }
-  if (val.includes("cos(")) {
-    val = val.replace("cos(", "Math.cos(");
-  }
-  if (val.includes("tan(")) {
-    val = val.replace("tan(", "Math.tan(");
-  }
-  if (val.includes("π")) {
-    val = val.replace("π", "Math.PI");
-  }
-  if (val.includes("x²")) {
-    val = val.replace("x²", "^2");
-  }
-  if (val.includes("x³")) {
-    val = val.replace("x³", "^3");
-  }
-  if (val.includes("xʸ")) {
-    val = val.replace("xʸ", "^");
-  }
-  if (val.includes("inv")) {
-    let num = val.match(/\d+(?=inv)/)[0];
-    val = val.replace(num + "inv", `1/${num}`);
-  }
-  if (val.includes("√(")) {
-    val = val.replace("√(", "Math.sqrt(");
-  }
-  if (val.includes("ʸ√(")) {
-    let match = val.match(/(\d+)ʸ√\((\d+)\)/);
-    if (match) {
-      let base = match[1];
-      let exponent = match[2];
-      val = val.replace(match[0], `Math.pow(${base}, 1/${exponent})`);
-    }
-  }
-  if (val.includes("exp(")) {
-    val = val.replace("exp(", "Math.exp(");
-  }
-  if (val.includes("log (")) {
-    val = val.replace("log (", "Math.log10(");
-  }
-  if (val.includes("ln (")) {
-    val = val.replace("ln (", "Math.log(");
-  }
-  if (val.includes("e^(")) {
-    val = val.replace("e^(", "Math.exp(");
-  }
-  if (val.includes("e")) {
-    val = val.replace("e", "Math.E");
-  }
-  if (val.includes("!")) {
-    let num = val.match(/\d+/)[0];
-    let fact = 1;
-    for (let i = 1; i <= num; i++) {
+  // Replace trigonometric functions
+  val = val.replace(/sin\(([^)]+)\)/g, function(match, p1) {
+    return `Math.sin(${p1} * Math.PI / 180)`;
+  });
+  val = val.replace(/cos\(([^)]+)\)/g, function(match, p1) {
+    return `Math.cos(${p1} * Math.PI / 180)`;
+  });
+  val = val.replace(/tan\(([^)]+)\)/g, function(match, p1) {
+    return `Math.tan(${p1} * Math.PI / 180)`;
+  });
+
+  // Replace π
+  val = val.replace("π", "Math.PI");
+
+  // Replace ² and ³
+  val = val.replace(/(\d+)²/g, function(match, p1) {
+    return `(${p1} * ${p1})`;
+  });
+  val = val.replace(/(\d+)³/g, function(match, p1) {
+    return `(${p1} * ${p1} * ${p1})`;
+  });
+
+  // Replace ʸ
+  val = val.replace(/(\d+)(ʸ)(\d+)/g, function(match, p1, p2, p3) {
+    return `Math.pow(${p1}, ${p3})`;
+  });
+
+  // Replace inv
+  val = val.replace(/(\d+)inv/g, function(match, p1) {
+    return `(1 / ${p1})`;
+  });
+
+  // Replace y√
+  val = val.replace(/(\d+)(y√)\((\d+)\)/g, function(match, p1, p2, p3) {
+    return `Math.pow(${p3}, 1 / ${p1})`;
+  });
+
+  // Replace √
+  val = val.replace(/√\(([^)]+)\)/g, function(match, p1) {
+    return `Math.sqrt(${p1})`;
+  });
+
+  // Replace E
+  val = val.replace(/(\d+)E(\d+)/g, function(match, p1, p2) {
+    return `${p1} * Math.pow(10, ${p2})`;
+  });
+
+
+  // Replace log
+  val = val.replace(/log\(([^)]+)\)/g, function(match, p1) {
+    return `Math.log10(${p1})`;
+  });
+
+  // Replace ln
+  val = val.replace(/ln\(([^)]+)\)/g, function(match, p1) {
+    return `Math.log(${p1})`;
+  });
+
+  
+  // Replace e
+  val = val.replace(/e/g, "Math.E");
+
+  // Replace e^
+  val = val.replace(/e\^/g, "Math.exp(");
+
+  // Handle factorial
+  while (val.includes("!")) {
+    let nums = val.match(/\d+(?=!)/)[0];
+    fact = 1;
+    for (let i = 1; i <= nums; i++) {
       fact *= i;
     }
-    val = val.replace(num + "!", fact);
+    val = val.replace(nums + "!", fact);
   }
-  
+
   try {
+    console.log(val);
     return eval(val);
   } catch (e) {
     return "Error: Invalid expression";
